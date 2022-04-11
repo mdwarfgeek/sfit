b7e190a59b8ae9f2f4f8d11fe69f831df257d45e
Jonathan Irwin <mdwarfgeek@users.noreply.github.com>
2022-04-11 12:06:17 +0100

    Modified range reduction to avoid compiling to x87 instructions. Further performance testing indicates gcc implements fmod using x87 when called with -ffast-math, so reimplemented the range reduction of the phase argument using rint which seems to be the fastest way to do it on my workstation.  This does roughly the same thing as the remainder() function so isn't exactly equivalent to the old fmod() but this is arguably a better way to do the range reduction anyway. It's mostly there to guard against any issues with large arguments in the implementation of the sin/cos or sincos functions.

--- sfit.c
+++ sfit.c
@@ -100,7 +100,12 @@ static inline int sfit_compute (struct sfit_info *inf,
     if(dosc) {
       /* Compute sin, cos */
       for(idp = 0; idp < lc->ndp; idp++) {
-        phi = fmod(v * lc->t[idp], 1.0);
+        /* Range reduction */
+        phi = v * lc->t[idp];
+        phi -= rint(phi);
+
+        /* Compute sin, cos, preferably simultaneously.
+           Results stored to arrays. */
         inline_bare_sincos(2*M_PI*phi, sinarg[idp], cosarg[idp]);
       }
 
